2645b19573d5791b5d0ad933ffc65c34ca328c92
Rohan Almeida <almeida@aarohan.biz>
2017-01-11 18:07:58 +0530

    added Benchmark test

--- binarytree/tree_test.go
+++ binarytree/tree_test.go
@@ -2,6 +2,7 @@ package tree
 
 import "testing"
 import "fmt"
+import "math/rand"
 
 func TestCreate(t *testing.T) {
 	root := CreateRoot(5)
@@ -26,4 +27,17 @@ func TestPrint(t *testing.T) {
 	root.AddNode(13)
 	root.PrintTree()
 	fmt.Println()
+}
+
+func BenchmarkAddNode(b *testing.B) {
+	root := CreateRoot(2)
+
+	for i := 0; i < b.N; i++ {
+		d := rand.Intn(1000)
+		//fmt.Printf("%d\n", d)
+		root.AddNode(d)
+	}
+
+	//root.PrintTree()
+	//fmt.Println()
 }
